37fa578f78af8d0699dd513c5d00b7f575e4c0c7
FlorianW <Florian.Woelki@telekom.de>
2019-06-04 10:08:43 +0200

    added basic header view

---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-navbar
+    fixed
+    dark
+  >
+    <div class="nav-center">
+      <v-navbar-item
+        v-for="navItem in navItems"
+        :key="navItem"
+      >
+        {{navItem}}
+      </v-navbar-item>
+    </div>
+  </v-navbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navItems: [
+        // TODO: Convert strings to icons
+        'Me',
+        'Timeline',
+        'Projects',
+        'Skills',
+        'Contact',
+      ],
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,51 @@
 <template>
   <div class="home">
-    <v-btn animated loadingLeft>Button Test</v-btn>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-space xlarge />
+    <v-space xlarge />
+    <v-container>
+      <img
+        src="../assets/logo.jpg"
+        alt="logo"
+        class="center"
+        width="250"
+        height="250"
+      >
+
+      <h5 class="center white">Florian Woelki</h5>
+
+      <v-divider
+        class="center"
+        short
+      />
+      <h6
+        class="center white"
+        style="font-weight: 100;"
+      >Developer</h6>
+      <v-divider
+        class="center"
+        short
+      />
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+
+<style>
+.home {
+  background-color: #212123;
+  width: 100%;
+  height: 100vh;
+}
+.home h5 {
+  margin-top: -5px;
+  text-transform: uppercase;
+}
+.home h6 {
+  text-transform: uppercase;
+}
+</style>
