b667e294ab50e101c9924bc6aee9e17dd1cc5d2b
FlorianWoelki <FlorianWoelki@gmx.de>
2019-08-10 14:41:27 +0200

    updated footer style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,6 @@
     title="Florian Woelki"
     subtitle="Copyright Florian Woelki"
   >
-    <h4>Make things happen.</h4>
+    <h4 class="white light">Make things happen.</h4>
   </v-footer>
 </template>
