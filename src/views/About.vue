b51bfcc0d3b6581def1637f5d6e555f669c09fac
FlorianWoelki <FlorianWoelki@gmx.de>
2019-08-15 12:28:05 +0200

    updated about view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,26 @@
 <template>
   <section id="about">
     <v-container>
-      <v-row>
+      <v-row level>
         <v-col c4>
           <div class="avatar">
-            <img src="../assets/logo.png">
+            <img src="../assets/profile_picture.png">
           </div>
         </v-col>
         <v-col c7 o1>
           <v-container>
             <h3>About Me</h3>
             <v-divider short />
-            <p>Some weird text about me.</p>
-            <p>Some weird text about me.</p>
-            <p>Some weird text about me.</p>
+            <p>
+              I am a passionate computer science student and software developer.
+            </p>
+            <p>
+              Web development, UI/UX or experimenting with artificial intelligence
+              are just a few of the things I enjoy.
+            </p>
+            <p>
+              I want to change the world with the help of AI.
+            </p>
           </v-container>
         </v-col>
       </v-row>
