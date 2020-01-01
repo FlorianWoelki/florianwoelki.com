1237c7d66f0517e3d08cefc6362463bed103582e
florianwoelki <florianwoelki@gmx.de>
2020-01-01 21:21:58 +0100

    Fix imports for vue-cirrus

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import VueCirrus from 'vue-cirrus';
 import App from './App.vue';
 import router from './router';
 
 import 'vue-cirrus/dist/vue-cirrus.css';
-import 'vue-cirrus';
+
+Vue.use(VueCirrus);
 
 Vue.config.productionTip = false;
 
