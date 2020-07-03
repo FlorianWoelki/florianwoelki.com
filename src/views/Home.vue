71c886d0ae29add1ee170ad0de9757fd583a857a
florianwoelki <florianwoelki@gmx.de>
2020-07-03 10:53:30 +0200

    feat(home): add first style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="home">
-    <button>Test Button</button>
+  <div class="bg-gray-200 relative">
+    <div class="container pt-16 pb-16">
+      <div class="absolute t-2 r-4 xs-hide">
+        <TriangleSVG is-right />
+      </div>
+      <div class="absolute b-4 xs-hide">
+        <TriangleSVG is-right />
+      </div>
+      <h1 class="mt-0 mb-1 font-bold">
+        Florian Woelki
+      </h1>
+      <p class="font-24 line-height-30">
+        Softwaredeveloper, Innovator, Creator
+      </p>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import TriangleSVG from '@/components/TriangleSVG.vue';
+
+@Component({
+  components: { TriangleSVG },
+})
+export default class Home extends Vue {
+
+}
+</script>
