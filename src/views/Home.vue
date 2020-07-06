230d56af9ec54a5aa45b4ee070b681680b1576d9
florianwoelki <florianwoelki@gmx.de>
2020-07-06 15:51:41 +0200

    feat(component): add statsbox component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="bg-gray-200 relative">
-    <div class="container pt-16 pb-16">
-      <div class="translate-l-full absolute top-2 right-4 xs-hide">
-        <TriangleSVG is-right />
+  <div>
+    <div class="bg-gray-200 relative">
+      <div class="container pt-16 pb-16">
+        <div class="translate-l-full absolute top-2 right-4 xs-hide">
+          <TriangleSVG is-right />
+        </div>
+        <div class="translate-r-full absolute bottom-2 left-0 xs-hide">
+          <TriangleSVG is-right />
+        </div>
+        <h1 class="mt-0 mb-1 font-bold">
+          Florian Woelki
+        </h1>
+        <p class="font-24 line-height-30">
+          Softwaredeveloper, Innovator, Creator
+        </p>
       </div>
-      <div class="translate-r-full absolute bottom-2 left-0 xs-hide">
-        <TriangleSVG is-right />
+    </div>
+
+    <div class="bg-gray-200 pt-3 pb-3">
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <StatsBox show-border />
+          </div>
+          <div class="col-6">
+            <StatsBox />
+          </div>
+        </div>
       </div>
-      <h1 class="mt-0 mb-1 font-bold">
-        Florian Woelki
-      </h1>
-      <p class="font-24 line-height-30">
-        Softwaredeveloper, Innovator, Creator
-      </p>
     </div>
   </div>
 </template>
@@ -20,9 +35,10 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import TriangleSVG from '@/components/TriangleSVG.vue';
+import StatsBox from '@/components/StatsBox.vue';
 
 @Component({
-  components: { TriangleSVG },
+  components: { TriangleSVG, StatsBox },
 })
 export default class Home extends Vue {}
 </script>
