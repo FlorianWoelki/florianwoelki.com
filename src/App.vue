d63159ee9354d7d319ac7c4f7a9e3a63b5dd0409
florianwoelki <florianwoelki@gmx.de>
2020-10-14 20:39:20 +0200

    feat: add content to home site

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="app">
+  <div
+    id="app"
+    class="antialiased"
+  >
     <router-view />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,29 @@
 <template>
-  <div class="py-16 bg-gray-100 overflow-x-hidden">
-    <div class="relative container mx-auto px-24">
-      <div class="hidden md:block absolute bottom-0 left-0 translate-l-full">
-        <TriangleSVG is-right />
+  <div class="py-32 bg-gray-800">
+    <div class="container px-16 mx-auto">
+      <div class="flex flex-col flex-wrap md:flex-row lg:items-center">
+        <div class="px-8 md:w-4/6">
+          <h1 class="mb-2 text-3xl font-semibold leading-none tracking-wider text-white uppercase md:text-2xl lg:text-3xl xl:text-5xl">
+            Florian Woelki
+          </h1>
+          <h2 class="text-2xl tracking-wide text-gray-500 uppercase">
+            Computer Science Major
+          </h2>
+        </div>
+        <div class="md:w-2/6">
+          <ul class="text-gray-400">
+            <li>email</li>
+            <li>github</li>
+          </ul>
+        </div>
       </div>
-      <div class="hidden md:block absolute top-0 right-0 translate-r-full">
-        <TriangleSVG is-right />
-      </div>
-      <h1 class="text-6xl">
-        test
-      </h1>
-      <button
-        class="text-sm shadow sm:shadow-none block w-full sm:w-auto rounded-lg bg-indigo-500
-        uppercase text-white tracking-wide font-semibold px-6 py-4"
-      >
-        GET THINGS DONE
-      </button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import TriangleSVG from '@/components/TriangleSVG.vue';
 
-@Component({
-  components: {
-    TriangleSVG,
-  },
-})
+@Component
 export default class Home extends Vue {}
 </script>
