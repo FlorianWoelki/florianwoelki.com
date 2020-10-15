6439f117f5987b3ff47bfbdd4bc2f968c714b1e8
florianwoelki <florianwoelki@gmx.de>
2020-10-15 09:27:23 +0200

    feat(page): add more second grid col

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="py-32 bg-gray-800">
       <div class="container px-16 mx-auto">
         <div class="flex flex-col flex-wrap md:flex-row lg:items-center">
-          <div class="px-8 md:w-4/6">
+          <div class="pr-8 md:w-4/6">
             <h1 class="mb-2 text-3xl font-semibold leading-none tracking-wider text-white uppercase md:text-2xl lg:text-3xl xl:text-5xl">
               Florian Woelki
             </h1>
@@ -12,19 +12,49 @@
             </h2>
           </div>
           <div class="md:w-2/6">
-            <ul class="text-lg text-gray-400">
+            <ul class="space-y-4 text-lg text-gray-400">
               <li class="flex items-center space-x-2">
                 <svg
                   class="w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
-                ><path
-                  fill-rule="evenodd"
-                  d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm0 2h10v7h-2l-1 2H8l-1-2H5V5z"
-                  clip-rule="evenodd"
-                /></svg>
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  />
+                </svg>
                 <span>florianwoelki@gmx.de</span>
+              </li>
+              <li class="flex items-center space-x-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-6 h-6"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path
+                    stroke="none"
+                    d="M0 0h24v24H0z"
+                    fill="none"
+                  />
+                  <!-- eslint-disable-next-line -->
+                  <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+                </svg>
+                <a
+                  href="https://github.com/FlorianWoelki"
+                  class="transition duration-300 ease-in-out hover:text-gray-500"
+                >
+                  <span>FlorianWoelki</span>
+                </a>
               </li>
             </ul>
           </div>
@@ -32,9 +62,9 @@
       </div>
     </div>
 
-    <div class="container grid grid-cols-2 px-16 mx-auto">
+    <div class="container grid grid-cols-2 gap-12 px-16 mx-auto mt-16">
       <div>
-        <Title class="mt-16">
+        <Title>
           About
         </Title>
 
@@ -47,7 +77,7 @@
           </p>
         </div>
 
-        <Title class="mt-10">
+        <Title class="mt-16">
           Skills
         </Title>
 
@@ -79,6 +109,26 @@
             </div>
           </div>
         </div>
+
+        <Title class="mt-16">
+          Personal Projects
+        </Title>
+      </div>
+
+      <div>
+        <Title>Education</Title>
+        <div class="ml-4">
+          <div class="mb-2 text-lg text-gray-900">
+            2017 - 2020
+          </div>
+          <Heading title="HfTL Dual">
+            B.S. Computer Science
+          </Heading>
+        </div>
+
+        <Title class="mt-16">
+          Work Experience
+        </Title>
       </div>
     </div>
   </div>
