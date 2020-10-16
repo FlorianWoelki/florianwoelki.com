c2b594157cae7365a0178797dbf9c09f4b08332f
florianwoelki <florianwoelki@gmx.de>
2020-10-16 08:58:10 +0200

    fix(page): responsive for header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,10 @@
       <div class="container px-16 mx-auto">
         <div class="flex flex-col flex-wrap md:flex-row lg:items-center">
           <div class="pr-8 md:w-4/6">
-            <h1 class="mb-2 text-3xl font-semibold leading-none tracking-wider text-white uppercase md:text-2xl lg:text-3xl xl:text-5xl">
+            <h1 class="mb-2 text-3xl font-semibold leading-none tracking-wider text-white uppercase md:text-4xl lg:text-5xl">
               Florian Woelki
             </h1>
-            <h2 class="text-2xl tracking-wide text-gray-500 uppercase">
+            <h2 class="text-lg tracking-wide text-gray-500 uppercase md:text-2xl">
               Computer Science Major
             </h2>
           </div>
