6a34043e2e43b77700e64ce0f9d974a24db7a4b0
florianwoelki <florianwoelki@gmx.de>
2020-10-16 09:08:05 +0200

    feat(comp): add responsive font size for title

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2 class="mb-4 text-3xl font-bold tracking-widest text-gray-800 uppercase">
+  <h2 class="mb-4 text-2xl font-bold tracking-widest text-gray-800 uppercase lg:text-3xl">
     <slot />
   </h2>
 </template>
