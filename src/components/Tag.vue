a4b0386817a3829e430a7a4aa2100e056aac35d0
florianwoelki <florianwoelki@gmx.de>
2020-11-28 17:00:41 +0100

    feat(comp): add selected state for tag

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="px-3 py-1 mt-1 ml-1 text-sm text-gray-600 bg-gray-200 rounded-lg">
+  <div
+    class="px-3 py-1 mt-1 mr-1 text-sm bg-gray-200 rounded-lg cursor-pointer"
+    :class="{ 'bg-indigo-300 text-indigo-600': selected, 'text-gray-600': !selected }"
+  >
     <slot />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    selected: Boolean,
+  },
+});
+</script>
