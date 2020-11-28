90eb66bb900d71c28f9bffac5cfc638c55c7321d
florianwoelki <florianwoelki@gmx.de>
2020-11-28 17:01:07 +0100

    feat(view): add new design for resume page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,120 +1,107 @@
 <template>
-  <div class="h-full pb-32 bg-gray-100">
-    <div class="py-32 bg-gray-800">
-      <div class="container px-16 mx-auto">
-        <div class="flex flex-col flex-wrap md:flex-row lg:items-center">
-          <div class="text-center md:w-4/6 sm:text-left sm:pr-8">
-            <h1 class="mb-2 text-3xl font-semibold leading-none tracking-wider text-white uppercase md:text-4xl lg:text-5xl">
-              Florian Woelki
-            </h1>
-            <h2 class="text-lg tracking-wide text-gray-500 uppercase md:text-2xl">
-              Undergraduate Computer Science Major
-            </h2>
-          </div>
-          <div class="mt-4 md:w-2/6 sm:mt-0">
-            <ul class="space-y-1 text-lg text-gray-400 md:space-y-4">
-              <li class="flex items-center justify-center space-x-2 sm:justify-start">
-                <svg
-                  class="w-6 h-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                  />
-                </svg>
-                <a
-                  href="mailto:florianwoelki@gmx.de"
-                  class="transition duration-300 ease-in-out hover:text-gray-500"
-                >
-                  <span>florianwoelki@gmx.de</span>
-                </a>
-              </li>
-              <li class="flex items-center justify-center space-x-2 sm:justify-start">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-6 h-6"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path
-                    stroke="none"
-                    d="M0 0h24v24H0z"
-                    fill="none"
-                  />
-                  <!-- eslint-disable-next-line -->
-                  <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
-                </svg>
-                <a
-                  href="https://github.com/FlorianWoelki"
-                  class="transition duration-300 ease-in-out hover:text-gray-500"
-                >
-                  <span>FlorianWoelki</span>
-                </a>
-              </li>
-            </ul>
+  <div class="h-full">
+    <div class="grid h-screen grid-cols-2 gap-12">
+      <div class="container px-16 py-16 mx-auto space-y-12 break-words bg-gray-100">
+        <!-- HEADER -->
+        <div class="flex items-center justify-between">
+          <h1 class="text-5xl font-light text-gray-800 uppercase">
+            Florian <span class="block text-6xl font-bold">Woelki</span>
+          </h1>
+          <div class="space-y-2 text-center">
+            <p class="text-gray-400">
+              Download resumé
+            </p>
+            <div class="flex items-center justify-between space-x-2">
+              <button
+                type="button"
+                class="px-6 py-2 font-medium leading-7 text-gray-400 transition duration-300 ease-in-out bg-transparent border border-gray-300 rounded-md focus:outline-none hover:bg-gray-300 hover:text-white"
+              >
+                PDF
+              </button>
+              <button
+                type="button"
+                class="px-6 py-2 font-medium leading-7 text-gray-400 transition duration-300 ease-in-out bg-transparent border border-gray-300 rounded-md focus:outline-none hover:bg-gray-300 hover:text-white"
+              >
+                Text
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
 
-    <div class="py-16 bg-gradient-to-r from-indigo-400 to-blue-500">
-      <div class="container px-16 mx-auto ">
-        <h3 class="text-2xl text-center text-white">
-          Download my resumé in other formats.
-        </h3>
-
-        <div class="flex items-center justify-center mt-4 space-x-2">
-          <button
-            type="button"
-            class="px-8 py-2 font-medium leading-7 text-white transition duration-300 ease-in-out bg-transparent border border-white rounded-md focus:outline-none hover:bg-white hover:text-gray-900"
-          >
-            PDF
-          </button>
-          <button
-            type="button"
-            class="px-8 py-2 font-medium leading-7 text-white transition duration-300 ease-in-out bg-transparent border border-white rounded-md focus:outline-none hover:bg-white hover:text-gray-900"
-          >
-            Text
-          </button>
+        <!-- SOCIALS -->
+        <div class="grid grid-cols-3 text-gray-400">
+          <div class="flex items-center space-x-2">
+            <div>
+              <svg
+                class="w-5 h-5"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <!-- eslint-disable-next-line -->
+                <path d="M12 0.297001C5.37 0.297001 0 5.67 0 12.297C0 17.6 3.438 22.097 8.205 23.682C8.805 23.795 9.025 23.424 9.025 23.105C9.025 22.82 9.015 22.065 9.01 21.065C5.672 21.789 4.968 19.455 4.968 19.455C4.422 18.07 3.633 17.7 3.633 17.7C2.546 16.956 3.717 16.971 3.717 16.971C4.922 17.055 5.555 18.207 5.555 18.207C6.625 20.042 8.364 19.512 9.05 19.205C9.158 18.429 9.467 17.9 9.81 17.6C7.145 17.3 4.344 16.268 4.344 11.67C4.344 10.36 4.809 9.29 5.579 8.45C5.444 8.147 5.039 6.927 5.684 5.274C5.684 5.274 6.689 4.952 8.984 6.504C9.944 6.237 10.964 6.105 11.984 6.099C13.004 6.105 14.024 6.237 14.984 6.504C17.264 4.952 18.269 5.274 18.269 5.274C18.914 6.927 18.509 8.147 18.389 8.45C19.154 9.29 19.619 10.36 19.619 11.67C19.619 16.28 16.814 17.295 14.144 17.59C14.564 17.95 14.954 18.686 14.954 19.81C14.954 21.416 14.939 22.706 14.939 23.096C14.939 23.411 15.149 23.786 15.764 23.666C20.565 22.092 24 17.592 24 12.297C24 5.67 18.627 0.297001 12 0.297001" />
+              </svg>
+            </div>
+            <a
+              href="https://github.com/FlorianWoelki"
+              target="_blank"
+            >/FlorianWoelki</a>
+          </div>
+          <div class="flex items-center space-x-2">
+            <div>
+              <svg
+                class="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <!-- eslint-disable-next-line -->
+              <path d="M20.447 20.452H16.893V14.883C16.893 13.555 16.866 11.846 15.041 11.846C13.188 11.846 12.905 13.291 12.905 14.785V20.452H9.351V9H12.765V10.561H12.811C13.288 9.661 14.448 8.711 16.181 8.711C19.782 8.711 20.448 11.081 20.448 14.166V20.452H20.447ZM5.337 7.433C4.193 7.433 3.274 6.507 3.274 5.368C3.274 4.23 4.194 3.305 5.337 3.305C6.477 3.305 7.401 4.23 7.401 5.368C7.401 6.507 6.476 7.433 5.337 7.433ZM7.119 20.452H3.555V9H7.119V20.452ZM22.225 0H1.771C0.792 0 0 0.774 0 1.729V22.271C0 23.227 0.792 24 1.771 24H22.222C23.2 24 24 23.227 24 22.271V1.729C24 0.774 23.2 0 22.222 0H22.225Z" />
+              </svg>
+            </div>
+            <a
+              href="https://www.linkedin.com/in/florian-woelki/"
+              target="_blank"
+            >/FlorianWoelki</a>
+          </div>
+          <div class="flex items-center space-x-1">
+            <div>
+              <svg
+                class="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              ><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" /><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" /></svg>
+            </div>
+            <a
+              href="mailto:florianwoelki@gmx.de"
+              target="_blank"
+            >FlorianWoelki@gmx.de</a>
+          </div>
         </div>
-      </div>
-    </div>
 
-    <div class="container px-16 mx-auto mt-16 break-words md:grid md:grid-cols-2 md:gap-12">
-      <div>
-        <Title>
-          About
-        </Title>
-
-        <div class="md:ml-4">
-          <Heading title="Florian Woelki">
-            Fullstack Softwareengineer
-          </Heading>
-          <p class="leading-7 text-gray-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere suscipit cumque reprehenderit doloribus iure dicta dolores autem, eligendi tempore et.
+        <!-- ABOUT -->
+        <div>
+          <h3 class="text-xl font-semibold tracking-wide text-gray-800 uppercase">
+            About
+          </h3>
+          <h4 class="-mt-1 text-gray-500 uppercase">
+            Fullstack Engineer
+          </h4>
+          <p class="mt-2 leading-7 text-gray-500">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, nesciunt deserunt quia laudantium aut quos aperiam, animi doloribus similique quidem voluptatem! Necessitatibus enim eaque nostrum possimus quo provident consectetur ut.
           </p>
         </div>
 
-        <Title class="mt-16">
-          Skills
-        </Title>
-
-        <div class="md:ml-4">
-          <Heading title="Languages">
-            Listed in descending order.
-          </Heading>
-
-          <div class="flex flex-wrap items-center">
+        <!-- SKILLS -->
+        <div>
+          <h3 class="text-xl font-semibold tracking-wide text-gray-800 uppercase">
+            Skills
+          </h3>
+          <h4 class="-mt-1 text-gray-500 uppercase">
+            Languages
+          </h4>
+          <div class="flex flex-wrap items-center mt-1">
             <Tag>JavaScript (ES6)</Tag>
             <Tag>TypeScript</Tag>
             <Tag>Go</Tag>
@@ -126,15 +113,10 @@
             <Tag>Scala</Tag>
             <Tag>Dart</Tag>
           </div>
-
-          <Heading
-            title="Frameworks / Technologies"
-            class="mt-8"
-          >
-            Not listed in any order.
-          </Heading>
-
-          <div class="flex flex-wrap items-center">
+          <h4 class="mt-8 text-gray-500 uppercase">
+            Frameworks / Technologies
+          </h4>
+          <div class="flex flex-wrap items-center mt-1">
             <Tag>Vue</Tag>
             <Tag>Nuxt</Tag>
             <Tag>GraphQL</Tag>
@@ -162,193 +144,21 @@
             <Tag>Pandas/Numpy/Matplotlib</Tag>
           </div>
         </div>
-
-        <Title class="mt-16">
-          Personal Projects
-        </Title>
-
-        <div class="md:ml-4">
-          <Heading title="VGQL">
-            <a
-              class="transition duration-300 ease-in-out hover:text-gray-500"
-              href="https://github.com/FlorianWoelki/vgql"
-            >github.com/FlorianWoelki/vgql</a>
-          </Heading>
-
-          <p class="leading-7 text-gray-700">
-            A custom made Vue GraphQL CLI written in TypeScript. With the help of this CLI
-            you can easily generate projects with the lovely preset of the Vue.js framework
-            and GraphQL. The CLI will setup everything you need for this fullstack application.
-          </p>
-
-          <Heading
-            title="Pneumonia Detection"
-            class="mt-8"
-          >
-            <a
-              class="transition duration-300 ease-in-out hover:text-gray-500"
-              href="https://github.com/FlorianWoelki/pneumonia_detection"
-            >github.com/FlorianWoelki/pneumonia_detection</a>
-          </Heading>
-
-          <p class="leading-7 text-gray-700">
-            A jupyter notebook that contains a exploratory analysis and transfer learning
-            convulutional neural network detecting if a xray scan has pneumonia or not.
-            Furthermore, I submitted this notebook to the official Kaggle competition.
-          </p>
-
-          <Heading
-            title="Vue Cirrus"
-            class="mt-8"
-          >
-            <a
-              class="transition duration-300 ease-in-out hover:text-gray-500"
-              href="https://github.com/FlorianWoelki/vue-cirrus"
-            >github.com/FlorianWoelki/vue-cirrus</a>
-          </Heading>
-
-          <p class="leading-7 text-gray-700">
-            A Vue component library for the Cirrus CSS framework. It is really simple to use
-            and lightweight as well.
-          </p>
-
-          <Heading
-            title="Daycademy"
-            class="mt-8"
-          >
-            <a
-              class="transition duration-300 ease-in-out hover:text-gray-500"
-              href="https://github.com/daycademy"
-            >github.com/daycademy</a>
-          </Heading>
-
-          <p class="leading-7 text-gray-700">
-            Daycademy is a completly free to use german academy for learning how to code.
-            On this self built end to end platform I teach how to code with interactive
-            coding trainings and quizes.
-          </p>
-
-          <Heading
-            title="YouTube Account"
-            class="mt-8"
-          >
-            <a
-              class="transition duration-300 ease-in-out hover:text-gray-500"
-              href="https://www.youtube.com/channel/UC18qytfIhR9cNEjUcgGLl3A?view_as=subscriber"
-            >Code mit FloW</a>
-          </Heading>
-
-          <p class="leading-7 text-gray-700">
-            In addition to the academy I have created a german speaking YouTube channel
-            for teaching how to code. These professional created videos are in the academy
-            available as well.
-          </p>
-
-          <Heading
-            title="First published Game"
-            class="mt-8"
-          >
-            <a
-              class="transition duration-300 ease-in-out hover:text-gray-500"
-              href="https://gamejolt.com/games/way-to-love/135095"
-            >Way To Love</a>
-          </Heading>
-
-          <p class="leading-7 text-gray-700">
-            This is my first ever published game. The main goal of this game is to
-            finish the jump and run with two characters that are mirrored. The game
-            was written in Java with LibGDX.
-          </p>
-
-          <Heading
-            title="Competition Game #59"
-            class="mt-8"
-          >
-            <a
-              class="transition duration-300 ease-in-out hover:text-gray-500"
-              href="https://gamejolt.com/games/way-to-love/135095"
-            >Revenge of the Clowns</a>
-          </Heading>
-
-          <p class="leading-7 text-gray-700">
-            I've developed this game for the Beansjam event, where you need to develop
-            a game with predefined themes. The goal is to throw or kill all the clowns from
-            the plattform and level yourself up with powerups and upgrades. This game was written
-            in pure Java without any additional library.
-          </p>
-        </div>
       </div>
 
-      <div class="mt-16 md:mt-0">
-        <Title>Education</Title>
-        <div class="md:ml-4">
-          <div class="mb-2 text-lg text-gray-900">
-            2017 - 2020
-          </div>
-          <Heading title="HfTL Dual">
-            B.S. Computer Science
-          </Heading>
-          <p class="leading-7 text-gray-700">
-            Bachelorthesis:
-            <i>Bluetooth Low Energy: A comparison between different machine learning algorithms with a variable object configuration in indoor areas.</i>
-          </p>
-        </div>
-
-        <Title class="mt-16">
-          Work Experience
-        </Title>
-        <div class="ml-4">
-          <div class="mb-2 text-lg text-gray-900">
-            October 2017 - Present
-          </div>
-          <Heading title="T-Systems International">
-            Fullstack Softwarearchitect
-          </Heading>
-          <ul class="ml-10 leading-7 text-gray-700 list-disc">
-            <li>Full-stack development on situational awareness dashboards using Angular/Vue, Spring and Scala/Java</li>
-            <li>Implemented RESTful API with SpringBoot secured with JWT authentication</li>
-            <li>Full-stack development on a real time indoor tracking service including UWB, LTE and BLE using Angular and Java with SpringBoot</li>
-            <li>Android app development with Kotlin for pairing a BLE beacon with a specific QR-Code</li>
-            <li>...</li>
-          </ul>
-
-          <div class="mt-8 mb-2 text-lg text-gray-900">
-            October 2019 - Present
-          </div>
-          <Heading title="Company Consulting Team">
-            IT Consultant
-          </Heading>
-          <ul class="ml-10 leading-7 text-gray-700 list-disc">
-            <li>Front-end inhouse work for a fully functional HR-Tool with Vue and TypeScript</li>
-            <li>Full-stack inhouse work for fully functional end to end polling tool written in Vue, TypeScript and Node.js</li>
-            <li>Front-end project for a customer by developing a contact form for a real estate company</li>
-            <li>Data Science project for a customer by merging and analyzing different data sources in Python</li>
-          </ul>
-        </div>
+      <div>
+        <h1>Projects</h1>
       </div>
-
-      <p class="ml-4 text-gray-600">
-        Originally designed by
-        <a
-          class="text-indigo-600"
-          href="https://www.stanleylim.me/resume/"
-          target="_blank"
-        >Stanley Lim</a>
-      </p>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Options } from 'vue-class-component';
-import Title from '@/components/Title.vue';
-import Heading from '@/components/Heading.vue';
 import Tag from '@/components/Tag.vue';
 
 @Options({
   components: {
-    Title,
-    Heading,
     Tag,
   },
 })
