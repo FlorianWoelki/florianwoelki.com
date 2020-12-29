e3c70849a2e694a1070f2c3f26cd417331074656
florianwoelki <florianwoelki@gmx.de>
2020-12-29 12:25:27 +0100

    feat(router): add dynamic title binding

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+import { nextTick } from 'vue';
 import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Year2020 from '../views/Year2020.vue';
@@ -7,17 +8,29 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      title: 'Resumé - Florian Woelki',
+    },
   },
   {
     path: '/2020',
     name: 'Year2020',
     component: Year2020,
+    meta: {
+      title: '2020 Year Review - Florian Woelki',
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+router.afterEach((to) => {
+  nextTick(() => {
+    document.title = to.meta.title || 'Florian Woelki';
+  });
 });
 
 export default router;
