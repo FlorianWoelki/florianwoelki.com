9a51450f5806de9ec427fffffc30d932df3be065
florianwoelki <florianwoelki@gmx.de>
2020-12-31 12:03:46 +0100

    feat(view): finishing up year 2020 review

--- src/views/Year2020.vue
+++ src/views/Year2020.vue
@@ -42,7 +42,7 @@ I was really excited to hop into the year 2020, but somehow things didn't go wel
 
 In this small post, I will close this year for myself.
 
-## Highlights
+# Highlights
 * **Health and Fitness —** I started intermittent fasting and got pretty shredded. In addition, I fell in love with home and bodyweight training.
 * **Career —** I got a job as a Softwarearchitect at T-Systems International GmbH and so far I am pretty happy with it.
 * **Education —** I've finished my Bachelor with a satisfactory grade and will continue learning in 2021.
@@ -53,12 +53,15 @@ In this small post, I will close this year for myself.
 * **Editing videos —** I've finally found my preferred way of editing videos after a lot of testing.
 * **Livestreaming —** I engaged with the viewers in four total livestreams. This isn't a lot, but this will change in 2021.
 
-## Personal
+# Personal
 2020 was a pretty testing like year where I tried to test a lot of things and experience new things.
 Honestly, I am not satisfied enough with the experiences had this year. But I will try to change this next year
 by building new things or simply trying new things.
 
-### Health and Fitness
+Besides that, I think I wasted a lot of time on the phone and watching social media or playing useless stuff.
+I will try to reduce the time spent on useless stuff like social media or playing in the upcoming year.
+
+## Health and Fitness
 Even if this year was a *i will stay inside* year I've used my body and my brain to experiment with a diet and with
 home training. I personally really enjoy lifting weights at home because you can adjust your training dynamically. For instance,
 you can focus on do on the one day a really intense workout and on the other day a more like fast workout with lighter weights but
@@ -68,7 +71,7 @@ In addition to my fitness that totally improved this year, I learned a lot about
 read some books about food and their nutrient. I really did not think that it would interest me, but I am more interest in brain health
 and health in general then ever before.
 
-### Education
+## Education
 This year I've finished my Bachelor Degree in Computer Science. Honestly, I've learned more at work than in university. Therefore,
 I will ask myself in the next year, if a Masters Degree in Computer Science is really useful. I might ask myself, if it is fullfilling
 and if it does make sense. Or is it more useful to earn a lot of experience at work and earn certificates about specific technologies.
@@ -85,27 +88,33 @@ are really interesting because you might learn new things and strengthen your cr
 Furthermore, I learned a lot about playing piano. This skill has nothing to do with programming or software development and is therefore
 a really nice hobby I would like to do more often.
 
-### Personal Plans for 2021
+## Open Source contribution
+I really contributed a lot to personal and private projects on GitHub. However I am not happy with the contribution in public open source
+repositories at all. I will try to focus more on that in the upcoming year. On GitHub I had 3.611 commits in 2020 and created over 15 projects.
+Furthermore, I started a lot of projects without even finishing it. I will try to finish them and therefore clean up my profile this year.
+
+## Personal Plans for 2021
 Here is a small list of my personal plans for the upcoming year:
 * Reading 12 books in 12 months
 * Finishing all unfinished projects on GitHub
 * Contribute to at least five open source projects
 * Read more scientific papers about Health and Machine Learning
 * Regular journaling about work stuff
+* Reduce social media and phone addiction to minimal 1-2 hours a day
 
-## Business
+# Business
 There were a lot of changes in this year, because I actually got my first fulltime job as a Softwarearchitect.
 Furthermore, I came up with a lot of business oppurtunities I will try to tackle this year. I had a lot of ideas which I will try to
 create this year.
 
-### Career
+## Career
 Right now I am a simple Softwarearchitect at T-Systems International GmbH. But besides that I try to build things in my freetime
 and educate the world about the future and programming.
 
 I have some plans for the year 2021 like starting a podcast or writing more often. I will try to handle all of that in the upcoming
 year. So it could be fun ;)
 
-#### Daycademy
+### Daycademy
 This year was a *redesign and create* year for Daycademy. I came up with the ideas this year and I made a lot of good progress.
 Right now, the academy is kind of in a release stadium and I am working towards it in the upcoming year. However, there were some
 fundamental changes about the design and the development. I redesigned the academy and the landing page from scratch and now I am pretty
@@ -116,7 +125,12 @@ in the browser was tough. I had to create a lot of bash scripts and think about 
 in the backend was to actually create a efficient and scalable infrastructure. Right now, I am not happy with it, but I will try to work it out
 in 2021.
 
-#### YouTube
+Here are some contribution stats on GitHub:
+* Started mid March
+* Created 13 repositories
+* 2.067 commits
+
+### YouTube
 2020 was a really good year in terms of numbers of my YouTube channel. I really couldn't believe that I would ever have 1000 subscribers
 on a YouTube channel. This was a magic number for me and I am really proud of it because I created educational videos constantly. In addition,
 I've found my style of editing videos, which was a tough process of trial and error. Below you can see this year YouTube stats.
@@ -132,9 +146,16 @@ The three most popular videos of 2020 were:
 
 So far, I am pretty happy how the YouTube channel evolves and I am seeing this as a hobby.
 
-### Financial
+## Financial
+I learned a lot about investing and economics in this year. However, I personally think that you cannot know enough in investing and econmics.
+Therefore, I will try to level up my game in econmics this year by attending to courses or reading books and learning about some economics.
+Personally, I think econmics is a interesting topic to learn more about.
+In addition, I will raise my investments per month to even save more money in stocks.
 
-### Business Plans for 2021
+I have no real business related financial plans or goals. All I know is that I want to reinvest in the company I will create to create even more
+and better products for the world. There are too much useless overpaid products in the market.
+
+## Business Plans for 2021
 Here is a small list of my business related plans for the upcoming year:
 * Try to livestream once a month on YouTube.
 * Creating the company and releasing Daycademy.
@@ -171,14 +192,14 @@ Let's jump into the cold water of 2021!`;
 .markdown a {
   @apply font-semibold text-gray-800 underline;
 }
+.markdown h1 {
+  @apply text-2xl font-bold mb-2 mt-10 text-gray-800;
+}
 .markdown h2 {
   @apply text-xl font-bold mb-2 mt-10 text-gray-800;
 }
 .markdown h3 {
   @apply leading-5 text-lg font-bold text-gray-800 mt-8 -mb-2;
-}
-.markdown h4 {
-  @apply leading-5 text-base font-bold text-gray-800 mt-8 -mb-2;
 }
 .markdown ul {
   @apply pl-10 list-disc;
