3454e7fb46905260fa280f74062f6cec2cd85562
FlorianWoelki <FlorianWoelki@gmx.de>
2021-07-13 11:59:45 +0200

    chore(view): optimize list 100 for darkmode

--- src/views/List100.vue
+++ src/views/List100.vue
@@ -1,15 +1,21 @@
 <template>
   <div
-    class="container max-w-4xl px-8 pt-8 pb-32 mx-auto break-words  lg:pt-16 md:px-16"
+    class="container h-screen max-w-4xl px-8 pt-8 pb-32 mx-auto break-words lg:pt-16 md:px-16"
   >
-    <h1 class="inline-block text-3xl border-b">List 100 - Bucket List</h1>
-    <p class="mt-4">
+    <h1 class="inline-block text-3xl border-b dark:text-gray-300">
+      List 100 - Bucket List
+    </h1>
+    <p class="mt-4 dark:text-gray-400">
       Things I want to do before I die. <i>(Progress as of April 30, 2021)</i>
     </p>
     <div
-      class="grid grid-cols-1 mt-6 space-y-2  sm:gap-4 sm:grid-cols-3 sm:space-y-0"
+      class="grid grid-cols-1 mt-6 space-y-2 sm:gap-4 sm:grid-cols-3 sm:space-y-0"
     >
-      <div v-for="columnIndex in 3" :key="columnIndex" class="space-y-2">
+      <div
+        v-for="columnIndex in 3"
+        :key="columnIndex"
+        class="space-y-2 dark:text-gray-400"
+      >
         <p
           v-for="(bucket, index) in list100Data.slice(
             (columnIndex - 1) * Math.ceil(list100Data.length / 3),
