1570d6da69208e27ce1be1a8168db7ca6df6e446
FlorianWoelki <FlorianWoelki@gmx.de>
2021-10-13 17:29:59 +0200

    refactor: md2vue plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 import { defineNuxtConfig } from 'nuxt3';
-import vitePluginMd2Vue from 'vite-plugin-md2vue';
+import md2vue from './vite/md2vue';
 
 export default defineNuxtConfig({
   css: ['@/assets/css/tailwind.css'],
   vite: {
-    plugins: [vitePluginMd2Vue()],
+    plugins: [md2vue()],
   },
 });
