1c8b88bb1e989cd3a2bf0c005e5d0f4625ab3bc4
FlorianWoelki <FlorianWoelki@gmx.de>
2021-10-15 10:19:38 +0200

    fix: tailwind issue with postcss and nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,16 @@ import md2vue from './vite/md2vue';
 
 export default defineNuxtConfig({
   css: ['~/assets/css/tailwind.css'],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
   vite: {
     plugins: [md2vue()],
   },
