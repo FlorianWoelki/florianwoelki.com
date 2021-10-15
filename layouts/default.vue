48522079880363b403de32088a75f43a161947df
FlorianWoelki <FlorianWoelki@gmx.de>
2021-10-15 22:07:02 +0200

    feat: add Navbar component and first header layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="antialiased">
+    <navbar />
     <slot />
   </div>
 </template>
+
+<script lang="ts">
+import Navbar from '../components/Navbar.vue';
+
+export default defineComponent({
+  components: {
+    Navbar,
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1 class="text-red-400">Hello World</h1>
+  <div
+    class="flex flex-col items-center justify-center space-y-2 bg-gray-900 py-28"
+  >
+    <h1 class="text-4xl font-bold text-white">Florian Woelki</h1>
+    <h2 class="text-xl text-gray-100">T-Shaped Fullstack Softwareengineer</h2>
+  </div>
 </template>
