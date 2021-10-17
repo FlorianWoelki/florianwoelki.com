6f041652386d5390573b1fe61edd1ad41aac7639
FlorianWoelki <FlorianWoelki@gmx.de>
2021-10-17 13:23:52 +0200

    feat(page): add first design for blog article

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -5,6 +5,9 @@ import marked from 'marked';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import Link from 'next/link';
 
+import ChevronLeftIcon from '../../icons/chevron-left.svg';
+import styles from './[slug].module.css';
+
 interface BlogPostProps {
   frontmatter: { [key: string]: string };
   slug: string;
@@ -16,15 +19,20 @@ const BlogPost: NextPage<BlogPostProps> = ({
   content,
 }) => {
   return (
-    <>
-      <Link href="/">
-        <a>Go back</a>
+    <div className="container max-w-6xl px-8 pt-8 pb-32 mx-auto break-words lg:pt-32 md:px-16">
+      <Link href="/" passHref>
+        <a className="inline-block">
+          <ChevronLeftIcon className="w-5 h-5 text-gray-400" />
+        </a>
       </Link>
-      <h1>{title}</h1>
-      <div>Posted on {date}</div>
-      {cover_image && <img src={cover_image} />}
-      <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
-    </>
+      <h1 className="mt-4 text-4xl font-bold tracking-tight">{title}</h1>
+      <p className="mt-1 mb-16 text-gray-600">Posted on {date}</p>
+
+      <div
+        className={styles.markdown}
+        dangerouslySetInnerHTML={{ __html: marked(content) }}
+      ></div>
+    </div>
   );
 };
 
