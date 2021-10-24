cbee462f6efb963e789d2669be26889a0aa61ce4
FlorianWoelki <FlorianWoelki@gmx.de>
2021-10-24 09:45:27 +0200

    chore(comp): responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/dist/client/router';
 import Link from 'next/link';
+import { useState } from 'react';
 import GithubIcon from '../icons/github.svg';
 import LinkedinIcon from '../icons/linkedin.svg';
+import MenuIcon from '../icons/menu.svg';
+import XIcon from '../icons/x.svg';
 
 interface NavbarItem {
   title: string;
@@ -23,10 +26,25 @@ const Navbar: NextPage = () => {
   ];
   const router = useRouter();
 
+  const [isMenuOpen, setMenuOpen] = useState<boolean>(true);
+
   return (
     <div className="fixed top-0 z-10 w-full py-4 bg-white border-b border-gray-100 backdrop-filter backdrop-blur-lg bg-opacity-30 firefox:bg-opacity-90">
       <div className="container flex items-center justify-between max-w-6xl px-8 mx-auto md:px-16">
-        <ul className="flex space-x-6">
+        {/* Small screens */}
+        <div
+          className="text-gray-700 md:hidden"
+          onClick={() => setMenuOpen(!isMenuOpen)}
+        >
+          {isMenuOpen ? (
+            <XIcon className="w-5 h-5"></XIcon>
+          ) : (
+            <MenuIcon className="w-5 h-5"></MenuIcon>
+          )}
+        </div>
+
+        {/* Large screens */}
+        <ul className="hidden md:flex md:space-x-6">
           {items.map((item, index) => (
             <li
               key={index}
@@ -56,6 +74,27 @@ const Navbar: NextPage = () => {
             </a>
           </li>
         </ul>
+
+        {isMenuOpen && (
+          <ul className="absolute inset-x-0 top-0 px-8 py-6 mx-2 space-y-4 bg-white rounded shadow mt-14 md:hidden">
+            {items.map((item, index) => (
+              <li
+                key={index}
+                className={
+                  router.route === item.link ||
+                  item.extraChecks?.includes(router.route)
+                    ? 'text-black'
+                    : 'text-gray-700'
+                }
+                onClick={() => setMenuOpen(false)}
+              >
+                <Link href={item.link} passHref>
+                  <a className="block">{item.title}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
