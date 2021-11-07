0ec7f8845aa4d8a7e38e5d2611c0dcd4b30d4fb1
FlorianWoelki <FlorianWoelki@gmx.de>
2021-11-07 15:54:09 +0100

    feat(page): add filter setting logic

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
+import { useState } from 'react';
 import Tag from '../components/Tag';
 
 enum ProgrammingLanguageTag {
@@ -39,28 +40,42 @@ const ProjectsPage: NextPage = (): JSX.Element => {
     isNaN(Number(v)),
   );
 
+  const [filter, setFilter] = useState<
+    string | TechnologyTag | ProgrammingLanguageTag | null
+  >(null);
+
+  const setFilterTag = (
+    tag: string | TechnologyTag | ProgrammingLanguageTag | null,
+  ): void => {
+    if (filter === tag) {
+      setFilter(null);
+    } else {
+      setFilter(tag);
+    }
+  };
+
   return (
     <div className="container max-w-6xl px-8 pt-24 pb-32 mx-auto break-words lg:pt-32 md:px-16">
       <h1 className="mt-4 text-4xl font-bold tracking-tight">Projects</h1>
-
       <p className="mt-6 mb-1 text-sm text-gray-500">
         Filter by programming languages:
       </p>
       <ul className="flex flex-wrap">
         {programmingLanguageTags.map((tag, index) => (
-          <li key={index}>
-            <Tag selected={false}>{tag}</Tag>
+          <li key={index} onClick={() => setFilterTag(tag)}>
+            <Tag selected={tag === filter}>{tag}</Tag>
           </li>
         ))}
       </ul>
       <p className="mt-4 mb-1 text-sm text-gray-500">Filter by technologies:</p>
       <ul className="flex flex-wrap">
         {technologyTags.map((tag, index) => (
-          <li key={index}>
-            <Tag selected={false}>{tag}</Tag>
+          <li key={index} onClick={() => setFilterTag(tag)}>
+            <Tag selected={tag === filter}>{tag}</Tag>
           </li>
         ))}
       </ul>
+      Filter: {filter}
     </div>
   );
 };
