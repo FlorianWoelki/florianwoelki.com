6279ecdffc19dca42ed80cecbf1a412f45b00429
FlorianWoelki <FlorianWoelki@gmx.de>
2021-11-10 10:07:23 +0100

    feat(page): add list100

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,71 +27,65 @@ const Home: NextPage<HomeProps> = ({ posts }) => {
   const randomHeight = Math.random() * 7 + 5;
 
   return (
-    <>
-      <div className="container max-w-6xl px-8 pb-32 mx-auto space-y-16 break-words md:px-16">
-        <div className="mb-20 space-y-12 text-center lg:text-left lg:grid lg:items-center lg:grid-cols-2 mt-28 lg:space-y-0">
-          <div className="space-y-2">
-            <h1 className="text-5xl font-bold text-gray-900">Florian Woelki</h1>
-            <h2 className="text-xl text-gray-800">
-              <span>
-                Fullstack Softwareengineer with special interest
-                <br />
-                in AI, Blockchain, and data
-              </span>
-            </h2>
-          </div>
-
-          <div className="flex flex-col items-center justify-between space-y-4 md:flex-row md:space-y-0">
-            <CountUpBubble
-              link="https://www.youtube.com/c/CodemitFloW"
-              title="YouTube Views"
-              amount={300000}
-              color="green"
-            />
-            <CountUpBubble
-              link="https://github.com/FlorianWoelki?tab=repositories"
-              title="Repositories"
-              amount={42}
-              color="blue"
-            />
-            <CountUpBubble
-              link="https://github.com/FlorianWoelki/florianwoelki.com/raw/master/pdf/new_resume.pdf"
-              title="Years of Experience"
-              amount={4}
-              color="purple"
-            />
-          </div>
+    <div className="container max-w-6xl px-8 pb-32 mx-auto space-y-16 break-words md:px-16">
+      <div className="mb-20 space-y-12 text-center lg:text-left lg:grid lg:items-center lg:grid-cols-2 mt-28 lg:space-y-0">
+        <div className="space-y-2">
+          <h1 className="text-5xl font-bold text-gray-900">Florian Woelki</h1>
+          <h2 className="text-xl text-gray-800">
+            <span>
+              Fullstack Softwareengineer with special interest
+              <br />
+              in AI, Blockchain, and data
+            </span>
+          </h2>
         </div>
 
-        <div>
-          <h3 className="mb-6 text-3xl font-bold text-gray-900">
-            Latest Blogs
-          </h3>
-          <div className="flex flex-col gap-10 mb-6 lg:flex-row">
-            {latestThreeBlogs.map((post, index) => (
-              <LatestBlogArticle
-                key={index}
-                post={post}
-                color={blogArticleColorMapping[index]}
-              ></LatestBlogArticle>
-            ))}
-          </div>
-          <Link href="/blog" passHref>
-            <a className="inline-flex items-center space-x-1 text-gray-400 transition duration-100 ease-in-out hover:text-gray-500">
-              <span>To all blog articles</span>
-              <ChevronRightIcon className="w-4 h-4" />
-            </a>
-          </Link>
-        </div>
-
-        <div>
-          <h3 className="mb-6 text-3xl font-bold text-gray-900">
-            Latest Videos
-          </h3>
-          <p>Coming Soon</p>
+        <div className="flex flex-col items-center justify-between space-y-4 md:flex-row md:space-y-0">
+          <CountUpBubble
+            link="https://www.youtube.com/c/CodemitFloW"
+            title="YouTube Views"
+            amount={300000}
+            color="green"
+          />
+          <CountUpBubble
+            link="https://github.com/FlorianWoelki?tab=repositories"
+            title="Repositories"
+            amount={42}
+            color="blue"
+          />
+          <CountUpBubble
+            link="https://github.com/FlorianWoelki/florianwoelki.com/raw/master/pdf/new_resume.pdf"
+            title="Years of Experience"
+            amount={4}
+            color="purple"
+          />
         </div>
       </div>
-    </>
+
+      <div>
+        <h3 className="mb-6 text-3xl font-bold text-gray-900">Latest Blogs</h3>
+        <div className="flex flex-col gap-10 mb-6 lg:flex-row">
+          {latestThreeBlogs.map((post, index) => (
+            <LatestBlogArticle
+              key={index}
+              post={post}
+              color={blogArticleColorMapping[index]}
+            ></LatestBlogArticle>
+          ))}
+        </div>
+        <Link href="/blog" passHref>
+          <a className="inline-flex items-center space-x-1 text-gray-400 transition duration-100 ease-in-out hover:text-gray-500">
+            <span>To all blog articles</span>
+            <ChevronRightIcon className="w-4 h-4" />
+          </a>
+        </Link>
+      </div>
+
+      <div>
+        <h3 className="mb-6 text-3xl font-bold text-gray-900">Latest Videos</h3>
+        <p>Coming Soon</p>
+      </div>
+    </div>
   );
 };
 
---
+++ pages/list100.tsx
@@ -0,0 +1,57 @@
+import { NextPage } from 'next';
+import list100 from '../assets/list100.json';
+
+const List100Page: NextPage = (): JSX.Element => {
+  return (
+    <div className="container max-w-6xl px-8 pt-24 pb-32 mx-auto break-words lg:pt-32 md:px-16">
+      <h1 className="mt-4 text-4xl font-bold tracking-tight">
+        List 100 - Bucket List
+      </h1>
+
+      <p className="mt-10">
+        Things I want to do before I die. <i>(Progress as of April 30, 2021)</i>
+      </p>
+
+      <ul className="grid grid-cols-1 mt-6 space-y-1 sm:grid-cols-2 sm:gap-4 sm:space-y-0">
+        <div className="space-y-1">
+          {list100.slice(0, list100.length / 2).map((bucket, index) => (
+            <li key={index} className="space-x-2">
+              <b
+                className={`${bucket.done ? 'text-blue-500' : 'text-red-500'}`}
+              >
+                {bucket.done ? (
+                  <del>{index + 1}</del>
+                ) : (
+                  <span>{index + 1}</span>
+                )}
+              </b>
+              <span>{bucket.name}</span>
+            </li>
+          ))}
+        </div>
+        <div className="space-y-1">
+          {list100
+            .slice(list100.length / 2, list100.length)
+            .map((bucket, index) => (
+              <li key={index} className="space-x-2">
+                <b
+                  className={`${
+                    bucket.done ? 'text-blue-500' : 'text-red-500'
+                  }`}
+                >
+                  {bucket.done ? (
+                    <del>{index + Math.floor(list100.length / 2) + 1}</del>
+                  ) : (
+                    <span>{index + Math.floor(list100.length / 2) + 1}</span>
+                  )}
+                </b>
+                <span>{bucket.name}</span>
+              </li>
+            ))}
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default List100Page;
