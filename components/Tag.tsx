85230052b55e0a91648db435b309d4889acb9de5
FlorianWoelki <FlorianWoelki@gmx.de>
2022-03-26 17:57:02 +0100

    fix(comp): tag dark mode

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -9,8 +9,7 @@ const Tag: NextPage<TagProps> = ({ selected, children }): JSX.Element => {
   const tagClasses = classes(
     {
       'bg-blue-300 text-blue-800': selected,
-      'bg-gray-200 text-gray-600 dark:text-gray-900 dark:bg-gray-500':
-        !selected,
+      'bg-gray-200 text-gray-600': !selected,
     },
     'px-3 py-1 mt-1 mr-1 text-sm rounded-lg cursor-pointer',
   );
